6113ab5864e7371823555c56213b1a8062e0b408
khuongduy-hoang <hoangduy00987@gmail.com>
2025-11-03 13:16:36 +0700

    initial commit

---
+++ migrate-mongo-config.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const config = {
+  mongodb: {
+    url: process.env.MONGO_URI || "mongodb://localhost:27017/dating_db",
+
+    databaseName: "dating_db",
+
+    options: {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  },
+
+  migrationsDir: "migrations",
+
+  changelogCollectionName: "changelog",
+
+  lockCollectionName: "changelog_lock",
+
+  lockTtl: 0,
+  migrationFileExtension: ".js",
+  useFileHash: false,
+
+  moduleSystem: 'commonjs',
+};
+
+module.exports = config;
---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+// src/app.module.ts
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { databaseConfig } from './config/database.config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({ useFactory: databaseConfig }),
+  ],
+  controllers: [AppController],
+  providers: [AppService], 
+})
+export class AppModule {}
