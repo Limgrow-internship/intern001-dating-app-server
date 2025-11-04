3077eaf527ec720b9f3e50df790f68c2da7f0eb3
khuongduy-hoang <hoangduy00987@gmail.com>
2025-11-04 15:47:25 +0700

    chore: initialize project structure(swager,compodoc,..

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('App') 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get("/hello")
+  @Get('/hello')
+  @ApiOperation({ summary: 'Lấy lời chào từ server' }) 
+  @ApiResponse({
+    status: 200,
+    description: 'Trả về lời chào từ AppService',
+    schema: {
+      example: { message: 'Hello World!' }, 
+    },
+  })
   getHello() {
     return { message: this.appService.getHello() };
   }
